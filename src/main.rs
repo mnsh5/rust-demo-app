5e2c59b93aaeae8510d782615dd42b19e1039320
mnsh4 <hugo.orlando2050@gmail.com>
2024-01-14 22:13:15 -0300

    🦕 add new features

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
-#[macro_use]
-extern crate rocket;
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
-fn hello() -> &'static str {
-    "Hello, world!"
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Pizzas from Buenos Aires!")
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello])
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(web::scope("/api/v1").service(hello))
+
+        // .route("/api/v1", web::get().to(get_pizzas))
+    })
+    .bind(("127.0.0.1", 8000))?
+    .run()
+    .await
 }
